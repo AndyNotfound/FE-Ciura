f821f7de3605b92d8b8560afb613b56441fbb485
AndyNotfound <Andy.Notfound@gmail.com>
2023-12-05 00:23:18 +0700

    Add: Remake of sidemenu

--- src/components/Sidemenu.jsx
+++ src/components/Sidemenu.jsx
@@ -1,35 +1,67 @@
+import { NavLink, useParams } from "react-router-dom";
 import { FaArrowRight, FaBookOpen, FaComments, FaQuestionCircle, FaUser } from "react-icons/fa";
 
 function SideMenu() {
+  const { username } = useParams();
   return (
-    <>
-      <div className="menu-section">
-        <div className=" menu-user">
-          <img className="img-menu-user" src="/profil-img.svg" alt="" />
-          <p>Novidaa98@gmail.com</p>
-        </div>
-        <div className="menu menu-profile">
-          <FaUser className="fa-brands" />
-          <a href="#">Profil</a>
-        </div>
-        <div className="menu menu-artikel">
-          <FaBookOpen className="fa-brands" />
-          <a href="#">Artikel</a>
-        </div>
-        <div className="menu menu-artikel">
-          <FaComments className="fa-brands" />
-          <a href="#">Forum</a>
-        </div>
-        <div className="menu menu-artikel">
-          <FaQuestionCircle className="fa-brands" />
-          <a href="#">Bantuan</a>
-        </div>
-        <div className="menu menu-artikel">
-          <FaArrowRight className="fa-brands" />
-          <a href="#">Keluar</a>
-        </div>
+    <div className="menu-section">
+      <div className=" menu-user">
+        <img className="img-menu-user" src="/profil-img.svg" alt="" />
+        <p>{username}</p>
       </div>
-    </>
+      <ul className="menu-item">
+        <li>
+          <NavLink
+            to={`/profiles/${username}`}
+            className={
+              ({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")
+            }
+          >
+            <FaUser className="fa-brands" />
+            Profil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/profiles/${username}/articles`}
+            className={
+              ({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")
+            }
+          >
+            <FaBookOpen className="fa-brands" />
+            Artikel
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/profiles/${username}/forum`}
+            className={
+              ({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")
+            }
+          >
+            <FaComments className="fa-brands" />
+            Forum
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/help`}
+            className={
+              ({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")
+            }
+          >
+            <FaQuestionCircle className="fa-brands" />
+            Bantuan
+          </NavLink>
+        </li>
+        <li>
+          <button className="sidemenu-logout">
+            <FaArrowRight className="fa-brands" />
+            Keluar
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
 export default SideMenu;
--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
-import "../styles/ProfilPage.css";
 import SideMenu from "../components/Sidemenu";
+import "../styles/ProfilPage.css";
 
 function ProfilePage() {
   return (
